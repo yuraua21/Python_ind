55f6b3a308e08f36a5654fb2b111ba62df870836
yuraua21 <105919483+yuraua21@users.noreply.github.com>
2022-05-27 11:54:46 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import  wikipedia 
 import telebot 
+import re
 
 bot = telebot.TeleBot('5310625340:AAEttM2dxt4DVQDUWCFYZhtIhBbG0OPrCIE')
 
@@ -8,77 +9,35 @@ wikipedia.set_lang("en")
 def getwiki(s):
     try:
         ny = wikipedia.page(s)
-
+        
         wikitext=ny.content[:1000]
-
+        
         wikimas=wikitext.split('.')
-
+        
         wikimas = wikimas[:-1]
-
+        
         wikitext2 = ''
-
+        
         for x in wikimas:
             if not('==' in x):
-
+                
                 if(len((x.strip()))>3):
                    wikitext2=wikitext2+x+'.'
             else:
                 break
-
-        wikitext2=re.sub('([^()])', '', wikitext2)
-        wikitext2=re.sub('([^()])', '', wikitext2)
-        wikitext2=re.sub('{[^{}]}', '', wikitext2)
-
+        
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        
         return wikitext2
-
+    
     except Exception as e:
-        return 'В енциклопедії нема інформації про це'
+        return   ' В енциклопедії нема інформації про це '
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Відправте мені любе слово, і я знайду його значення у Wikipedia')
-
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, getwiki(message.text))
-
-bot.polling(none_stop=True, interval=0)import telebot, wikipedia, re
-
-bot = telebot.TeleBot('5310625340:AAEttM2dxt4DVQDUWCFYZhtIhBbG0OPrCIE')
-
-wikipedia.set_lang("en")
-
-def getwiki(s):
-    try:
-        ny = wikipedia.page(s)
-
-        wikitext=ny.content[:1000]
-
-        wikimas=wikitext.split('.')
-
-        wikimas = wikimas[:-1]
-
-        wikitext2 = ''
-
-        for x in wikimas:
-            if not('==' in x):
-
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-
-        wikitext2=re.sub('([^()])', '', wikitext2)
-        wikitext2=re.sub('([^()])', '', wikitext2)
-        wikitext2=re.sub('{[^{}]}', '', wikitext2)
-return wikitext2
-
-    except Exception as e:
-        return 'В енциклопедії нема інформації про це'
-
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Відправте мені любе слово, і я знайду його значення у Wikipedia')
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
